bf3a7edad1bbc0a42affab148970f38f80986a63
Chen_Guo <54894516+CirrusG@users.noreply.github.com>
2021-11-01 01:26:39 -0400

    Update query.py

--- src/cli/query.py
+++ src/cli/query.py
@@ -4,7 +4,8 @@
 # resource:
 # Basic module usage: https://www.psycopg.org/docs/usage.html (query params, adaptation of python values to sql types, etcs)
 # SQL string composition: https://www.psycopg.org/docs/sql.html#sql-objects
-# 
+# TODO list:
+# - search song by criteria
 
 import psycopg2
 # just for ease of changing the database(local -> starbug)
@@ -42,10 +43,11 @@ def get_result(curs):
     result = None
     try:
         result = []
+        # output of fetch is a array, else it is None
+        # the array contains one or more values, do not use row[0], check length firstly
         row = curs.fetchone()
         while row is not None:
             result.append(row)
-            print(row)
             row = curs.fetchone()
             # psycopg2.RaiseException
     except (Exception, psycopg2.Error) as error:
@@ -67,6 +69,7 @@ def email_exists(email):
 
 def pass_correct(username, password):
     # check if password correct
+    # since it need to compare if password correct, check if none does not work
     # since the user pass login check, no require to check if password is None
     # 🎉 PASS TEST
     conn = curs = result = None
@@ -76,7 +79,11 @@ def pass_correct(username, password):
         curs.execute(
             "SELECT password FROM Account WHERE username=%s", (username,))
         row = curs.fetchone()
-        result = (row == password)
+        # print(row) #it is a array
+        if row is not None:
+            row = row[0]
+            # only row is not None, comparasion is meaningful
+            result = (row == password)
     except(Exception, psycopg2.DatabaseError) as error:
         print("query.pass_correct(ERROR)", error)
     finally:
@@ -94,7 +101,7 @@ def register_user(uname, password, first, last, email):
             conn = starbug.connect()
             curs = conn.cursor()
             query = "INSERT INTO Account VALUES (%s, %s, %s, %s, %s)"
-            curs.execute(query, (uname, password, first, last, email,))
+            curs.execute(query, (uname, email, password, first, last,))
             conn.commit()
         except (Exception, psycopg2.Error) as error:
             print("query.register_user(ERROR):", error)
@@ -106,6 +113,8 @@ def register_user(uname, password, first, last, email):
 
 def collec_exists(user, c_name):
     # check if collection exist based on username, and collection name
+    # in the query, has condition to check by collection name
+    # just check if None or not to know the existence
     # 🎉 PASS TEST
     conn = curs = row = None
     try:
@@ -140,6 +149,7 @@ def add_collec(username, name):
 
 def list_collec(username):
     # returns list of collecs belonging to users username
+    # need to handle array message returned
     # 🎉 PASS TEST
     conn = curs = result = None
     try:
@@ -210,6 +220,8 @@ def show_collec(username, collec):
     return result
 
 
+# criteria
+# 1.
 # def song_list(criteria, value, username):
 #     # select criteria from table where criteria like value
 #     result = None
@@ -308,18 +320,210 @@ def show_collec(username, collec):
 #     finally:
 #         starbug.disconnect(conn, curs)
 
+# friend: find friend by email -> output info if find -> add friend by select username
+def find_friend_e(email):
+    # ✨ pass test!
+    # find friend by email, print username, first&last name if find
+    # return username if has, otherwise None
+    # when the return is not None, cli could query if want to add this friend
+    conn = curs = friend = None
+    try:
+        conn = starbug.connect()
+        curs = conn.cursor()
+        query = """select username, first_name, last_name from account where username = %s"""
+        curs.execute(query, (email,))
+        result = get_result(curs)
+        # email is unique, so the output only be [1][1]
+        # example output: [('ta', 'Taya', 'Andersen')]
+        # if no result: []
+        # print(result)
+        if len(result) == 1 and len(result[0]) == 3:
+            friend = result[0][0]
+    except (Exception, psycopg2.Error) as error:
+        print("find_friend(ERROR):", error)
+    finally:
+        starbug.disconnect(conn, curs)
+        return friend
+
+
+def find_friend(username):
+    # repurposed find_friend function to be used w username instead of email
+    conn = curs = friend = None
+    try:
+        conn = starbug.connect()
+        curs = conn.cursor()
+        query = """select username, first_name, last_name from account where username = %s"""
+        curs.execute(query, (username,))
+        result = get_result(curs)
+        # email is unique, so the output only be [1][1]
+        # example output: [('ta', 'Taya', 'Andersen')]
+        # if no result: []
+        # print(result)
+        if len(result) == 1 and len(result[0]) == 3:
+            friend = result[0][0]
+    except (Exception, psycopg2.Error) as error:
+        print("find_friend(ERROR):", error)
+    finally:
+        starbug.disconnect(conn, curs)
+        return friend
+
+
+def follow_friend(friend, username):
+    # ✨ pass test!
+    # @parm friend: friend to follow (following)
+    # @param username: current user in the cli that want to follow friend(follower)
+    # do not need to check user username since entered application
+    # but need to record 'username' as global value in the cli since it need to use many times
+    conn = curs = result = None
+    try:
+        conn = starbug.connect()
+        curs = conn.cursor()
+        if (if_exist('account', 'username', friend)):
+            query = """insert into follows (following, follower) values (%s, %s)"""
+            curs.execute(query, (friend, username, ))
+            conn.commit()
+            result = True
+            # TODO may need double check
+    except (Exception, psycopg2.Error) as error:
+        print("follow_friend(ERROR):", error)
+    finally:
+        starbug.disconnect(conn, curs)
+    return result
+
+
+def unfollow_friend(friend, username):
+    # ✨ pass test
+    # copied the follow_friend code and changed INSERT to DELETE, may need some tweaking
+    conn = curs = result = None
+    try:
+        conn = starbug.connect()
+        curs = conn.cursor()
+        if (if_exist('account', 'username', friend)):
+            query = """DELETE FROM follows where following = %s and follower = %s"""
+            curs.execute(query, (friend, username, ))
+            conn.commit()
+            result = True
+            # TODO may need double check
+    except (Exception, psycopg2.Error) as error:
+        print("unfollow_friend(ERROR):", error)
+    finally:
+        starbug.disconnect(conn, curs)
+    return result
+
+
+def show_friend_list(username):
+    # ✨ pass test
+    # TODO: need to parse output
+    conn = curs = list = None
+    try:
+        conn = starbug.connect()
+        curs = conn.cursor()
+        query = """select account.first_name, account.last_name 
+        from account inner join follows on account.username = follows.following
+        where follows.follower = %s"""
+        curs.execute(query, (username, ))
+        list = get_result(curs)
+        # NOTE: if it is empty array, but not same as none, need to check length of this
+        # just test output, the result will handle in the cli.py
+        # array or none
+        for row in get_result(curs):
+            print(row)
+    except (Exception, psycopg2.Error) as error:
+        print("find_friend(ERROR):", error)
+    finally:
+        starbug.disconnect(conn, curs)
+        return list
+
+
+def play_song(username, songID):
+    # ✨ pass test
+    # play song by username, songID (after looking up song, select the id to play)
+    # return song info
+    # add play record
+    conn = curs = song = None
+    try:
+        conn = starbug.connect()
+        curs = conn.cursor()
+        query = """select * from song where songID = %s"""
+        curs.execute(query, (songID, ))
+        list = get_result(curs)
+        if (len(list[0]) > 0):
+            song = list[0]
+            query = """insert into plays(username, songID) values (%s, %s)"""
+            curs.execute(query, (username, songID,))
+            conn.commit()
+            # test record in plays
+            # NOTE: datatime need to be handled by extracting to get day, month, year and time
+            curs.execute(
+                "select playDateTime from plays where username = %s and songID = %s", (username, songID,))
+            for each in get_result(curs):
+                print(each)
+    except (Exception, psycopg2.Error) as error:
+        print("find_friend(ERROR):", error)
+    finally:
+        starbug.disconnect(conn, curs)
+        return song
+
+def play_album(username, albumID):
+    # ✨ pass test
+    # call play_song to get a list of song info
+    conn = curs = None
+    try:
+        conn = starbug.connect()
+        curs = conn.cursor()
+        query = """select song.songID from song 
+                join features on song.songID = features.songID
+                where features.albumID = %s"""
+        curs.execute(query, (albumID,))
+        list = get_result(curs)
+        for each in list:
+            if len(each) == 1:
+                play_song(username, each[0])
+    except (Exception, psycopg2.Error) as error:
+        print("find_friend(ERROR):", error)
+    finally:
+        starbug.disconnect(conn, curs)
+
+def play_collec(username, collecID):
+    # ✨ pass test
+    conn = curs = None
+    try:
+        conn = starbug.connect()
+        curs = conn.cursor()
+        query = """select song.songID from song 
+                join added_to on song.songID = added_to.songID
+                where added_to.collectionID = %s"""
+        curs.execute(query, (collecID,))
+        list = get_result(curs)
+        for each in list:
+            if len(each) == 1:
+               play_song(username, each[0])
+    except (Exception, psycopg2.Error) as error:
+        print("find_friend(ERROR):", error)
+    finally:
+        starbug.disconnect(conn, curs)
+
 
 def main():
     # test connect and query
     #print(collec_exists('pb', 'My Favorite'))
     #print(if_exist("account", "password", "password"))
+    #print(pass_correct('pb', 'password'))
     # rename_collec('pb', 'One night', 'Halloween')
     #add_collec('pb', 'wow')
-    #list_collec('pb')
+    # list_collec('pb')
     #delete_collec('pb', 'wow')
-    #list_collec('pb')
+    # list_collec('pb')
     #show_collec('pb', 'collection')
-    return
+    #register_user('yl', 'hanna', 'lee', 'yun', 'ly@mail.com')
+    # print(find_friend("ly@mail.com"))
+    #print(follow_friend(find_friend("ly@mail.com"), 'pb'))
+    # NOTE: it is empty array, but not same as none, need to check length of this
+    #print(unfollow_friend('yl', 'pb'))
+    # show_friend_list('pb')
+    #print(play_song('pb', 'song100'))
+    #play_album('pb', 'album1000')
+    #play_collec('pb', 'collection1')
 
 
 if __name__ == "__main__":
