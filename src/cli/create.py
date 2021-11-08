c5cd94c6b553dd9a5449e0734badf91dc979c0eb
Chen_Guo <54894516+CirrusG@users.noreply.github.com>
2021-11-08 01:52:30 -0500

    Update create.py

--- src/cli/create.py
+++ src/cli/create.py
@@ -2,6 +2,7 @@ import psycopg2
 from psycopg2 import sql
 import starbug
 import read
+# create - insert
 
 def login_user(username):
     # login user (add record into accessdatestimes)
@@ -10,7 +11,7 @@ def login_user(username):
     result = False
     conn = curs = None
     try:
-        conn = starbug.connect(server)
+        conn = starbug.connect()
         curs = conn.cursor()
         curs.execute(
             "insert into user_accessdatestimes(username) values (%s)", (username,))
@@ -39,6 +40,26 @@ def register_user(username, password, first, last, email):
             result = True
     except (Exception, psycopg2.Error) as error:
         print("create.register_user(ERROR):", error)
+    finally:
+        starbug.disconnect(conn, curs)
+    return result
+
+def follow_friend(username, friend):
+    # @parm friend: friend to follow (following)
+    # @param username: current user in the cli that want to follow friend(follower)
+    # do not need to check user username since entered application
+    # but need to record 'username' as global value in the cli since it need to use many times
+    conn = curs = result = None
+    try:
+        conn = starbug.connect()
+        curs = conn.cursor()
+        if read.verify_username(friend):
+            query = """insert into follows (following, follower) values (%s, %s)"""
+            curs.execute(query, (friend, username, ))
+            conn.commit()
+            result = get_user_info(friend, False)
+    except (Exception, psycopg2.Error) as error:
+        print("follow_friend(ERROR):", error)
     finally:
         starbug.disconnect(conn, curs)
     return result
