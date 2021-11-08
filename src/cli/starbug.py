a1a87db571bb5767b55010f7e21989a8a60144de
Chen_Guo <54894516+CirrusG@users.noreply.github.com>
2021-11-08 01:53:45 -0500

    Update starbug.py

--- src/cli/starbug.py
+++ src/cli/starbug.py
@@ -1,90 +1,92 @@
 # file: database.py
+# author: Datamazing (group 8)
 # demo: SSHTunnelForwarder: connect to server/host 
 #       -> Psycopg2: connect to RDBMS(PostgreSQL)  
 #       -> SQL: modify database
 #       -> Psycopg2: fetch, commit modification to database
-#       -> close curor and conenct with database
+#       -> close cursor and connect with database
+# docs: https://sshtunnel.readthedocs.io/en/latest/
+from datetime import datetime
 import psycopg2
-
-
-username = "cg2531"
-password = "@Nanno666(^)Chicha"
-dbName = "p320_10"
+import sshtunnel
 from sshtunnel import SSHTunnelForwarder
 
+
+def get_db_name():
+    # group database name
+    # DON'T FORGET TO REMOVE THIS
+    dbName = "p320_10"
+    return dbName
+
+
+def get_password():
+    # ssh/database password
+    # DON'T FORGET TO REMOVE THIS
+    password = ""
+    return password
+
+
+def get_username():
+    # ssh/database username
+    # DON'T FORGET TO REMOVE THIS
+    username = ""
+    return username
+
+
 def conn_server():
-    server = SSHTunnelForwarder(('starbug.cs.rit.edu', 22),
-                            ssh_username=username,
-                            ssh_password=password,
-                            remote_bind_address=('localhost', 5432))
-        #server.start()
-    #print("SSH tunnel established")
+    # connect to starbug server
+    # @return server pointer
+    server = None
+    try:
+        server = SSHTunnelForwarder(('starbug.cs.rit.edu', 22),
+                                    ssh_username=get_username(),
+                                    ssh_password=get_password(),
+                                    remote_bind_address=('localhost', 5432))
+        server.start()
+        #print(datetime.now(), "SSH tunnel established")
+    except sshtunnel.BaseSSHTunnelForwarderError as error:
+        print("starbug.conn_server(Exception):", error)
     return server
 
+
 def disconn_server(server):
+    # close starbug server
     server.close()
+    #print(datetime.now(), "SSH tunnel closed")
 
 
-def connect(server):
-    # info to connect database
+def connect():
+    # connect database
+    # @return database connect pointer
+    global server
+    server = conn_server()
     conn = None
-
     try:
-        #Connect to the server hosting the database by SSH
+        params = {
+            'database': get_db_name(),
+            'user': get_username(),
+            'password': get_password(),
+            'host': 'localhost',
+            'port': server.local_bind_port
+        }
 
-            # parameter to store information required to connect db
-            params = {
-                'database': dbName,
-                'user': username,
-                'password': password,
-                'host': 'localhost',
-                'port': server.local_bind_port
-                #'port': 5432
-            }
-
-            # connect to the db
-            conn = psycopg2.connect(**params)
-            #print("Database connection established")
-                #print("postgreSQL:", conn.get_dsn_parameters(), "\n")
-
-                # cursor
-                #curs = conn.cursor()
-
-                # execute query
-                #curs.execute("SELECT version();")
-
-                # fetch all/remaining rows of a query result set and returns a list
-                # of tumbles. if no row, return a empty list
-                # execute -> fetchall
-                # ==> Here: the result of "SELECT version();"
-                #rows = curs.fetchall()
-                #print(f"curs.fetchall(): {curs.fetchall()}")
-
-                # make the changes to the database persistent; currently is no change(such as table creation)
-                #conn.commit()
-    except (Exception, psycopg2) as error:
-        print(f"starbug.py(Exception): {error}")
+        conn = psycopg2.connect(**params)
+        #print(datetime.now(), "Database connection established")
+    except psycopg2.Error as error:
+        print(f"starbug.connect(Exception): {error}")
     return conn
-        
+
 
 def disconnect(conn, curs):
     try:
-        if conn is not None:
+        if (conn is not None) and (curs is not None):
             # close cursor
             curs.close()
             # close connect
             conn.close()
-            #print("Database connection is closed")
+            # print("Database connection is closed")
     except (Exception, psycopg2.Error) as error:
-       print(f"Catch Exception: {error}")
-
-# test
-# server = conn_server()
-# server.start()
-# conn = connect()
-# curs = conn.cursor()
-# #curs.execute("select usename, count(*) as count from pg_stat_activity where datname like 'p320\__' group by usename order by count desc")
-# curs.execute("select version();")
-# print(query.get_result(curs))
-# disconnect(conn, curs)
-# server.stop()
+        print(f"starbug.disconnect(Exception): {error}")
+    finally:
+        disconn_server(server)
+        #print(f"{datetime.now()} Datebase connection broken")
